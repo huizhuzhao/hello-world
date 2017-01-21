f27bd3e91f9f95089310eefdbac503d8be80db78
huizhu <zhaohuizhu@ruc.edu.cn>
2017-01-21 20:02:42 +0800

    update

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# Created Time: 2017年01月21日 星期六 19时30分16秒
+
+def main():
+	w = 'learning'
+	print w.endswith('ing')
+
+
+
+if __name__ == '__main__':
+	main()
